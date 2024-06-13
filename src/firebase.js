baa4c6ee5ef7edb9a1755e7fcf75b3ad480ef865
Sahil Kalkal <sahilkalkal108@gmail.com>
2024-06-13 16:42:30 +0530

    env file setup

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import "firebase/compat/auth"
 import "firebase/compat/firestore"
 
 firebase.initializeApp({
-  apiKey: "AIzaSyCdHUfflhuggk40qmMoAFYPvGtR-9fl9Xs",
-  authDomain: "socioease.firebaseapp.com",
-  projectId: "socioease",
-  storageBucket: "socioease.appspot.com",
-  messagingSenderId: "205318143297",
-  appId: "1:205318143297:web:2f4474c8624cb79a2935c0"
+  apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID
 })
 
 const auth = firebase.auth()
